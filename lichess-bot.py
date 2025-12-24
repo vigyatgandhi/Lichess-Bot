a058493850787dde80feee3a3a11a1c19d5ae136
Vigyat Gandhi <vigyat.gandhi@gmail.com>
2025-12-24 18:48:32 +0530

    Update documentation, fix typo

--- lichess-bot.py
+++ lichess-bot.py
@@ -2,7 +2,7 @@
 Lichess Chess Bot
 =================
 
-Copyright (C) 2025 Vigyat Gandhit
+Copyright (C) 2025 Vigyat Gandhi
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
